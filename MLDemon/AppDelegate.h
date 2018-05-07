fd2edaac1caaf4a6a6878766d6ec077e551daee0
Qu,Ke <ke>
2018-05-07 16:26:44 +0800

    Initial Commit

---
+++ MLDemon/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLDemon
+//
+//  Created by Qu,Ke on 2018/5/7.
+//  Copyright © 2018年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLDemon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MLDemon
+//
+//  Created by Qu,Ke on 2018/5/7.
+//  Copyright © 2018年 baidu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
